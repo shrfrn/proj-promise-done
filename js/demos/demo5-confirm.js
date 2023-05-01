5832ef9aed80337b26f9e7a2f2fe59f43404b793
shrfrn <shrfrn@gmail.com>
2023-05-01 21:30:03 +0300

    Add solution to demo5-confirm

--- js/demos/demo5-confirm.js
+++ js/demos/demo5-confirm.js
@@ -6,4 +6,9 @@ doConfirm('Are your sure?').then(isConfirmed => {
     console.log('User decided:', isConfirmed)
 })
 
-
+function doConfirm(msg) {
+    return new Promise((resolve) => {
+        const isConfirmed = confirm(msg)
+        resolve(isConfirmed)
+    })
+}
