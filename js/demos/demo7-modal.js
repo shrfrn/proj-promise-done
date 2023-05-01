c8135d54760f13094ff25ec14a6a39597239ea23
shrfrn <shrfrn@gmail.com>
2023-05-01 21:24:02 +0300

    Add solution to demo7-modal.js

--- js/demos/demo7-modal.js
+++ js/demos/demo7-modal.js
@@ -1,25 +1,27 @@
 'use strict'
 
 // TODO: Build a "Promised Based" dialog modal (like SWAL)
-// onRemoveAll()
-
 
 var keepResolve
 
-function onUserDecision(userDecision) {
-    console.log('User Clicked:', userDecision)
-    document.querySelector('.modal').hidden = true
+function onAskUser() {
+    askUser()
+        .then(userDecision => {
+            console.log('User Decided', userDecision)
+        })
 }
 
 function askUser() {
     document.querySelector('.modal').hidden = false
     // TODO: we cannot return a resolved promise here
-    return Promise.resolve(true)
+    // return Promise.resolve(true)
+    return new Promise(resolve => {
+        keepResolve = resolve
+    })
 }
 
-function onAskUser() {
-   askUser()
-   .then(userDecision => {
-       console.log('User Decided', userDecision)
-   })
+function onUserDecision(userDecision) {
+    console.log('User Clicked:', userDecision)
+    document.querySelector('.modal').hidden = true
+    keepResolve(userDecision)
 }
